d6f71da49cd2e8d823f9ecc80d76629f9d00da4c
Amyas Merivale <merivale@gmail.com>
2018-02-25 10:17:46 +0100

    finished refactoring texts and js

--- _includes/js/regex.js
+++
@@ -1,7 +0,0 @@
-/* create a regular expression from user input */
-(() => {
-
-  return query =>
-    new RegExp(`\\b(${query})\\b`, 'gi');
-
-})();
--- assets/hume.js
+++ assets/hume.js
@@ -1,76 +1,149 @@
 ---
 layout: null
 ---
-const texts = {{ site.data.texts | jsonify }};
-const sections = {{ site.data.sections | jsonify }};
-const regex = {% include js/regex.js %}
-const dom = {% include js/dom.js %}
-const format = {% include js/format.js %}
+/* basics */
+const texts = {{ site.data | jsonify }};
+const $ = id => document.getElementById(id);
+const path = window.location.pathname.replace(/(^\/|\/$)/g, '').split('/');
+const text = $('text') ? texts[$('text').getAttribute('data-text')] : texts[path.slice(1).join('-')];
 
-dom.toggles.forEach((node) => {
-  const target = document.getElementById(node.getAttribute('data-toggle'));
-  node.addEventListener('click', (event) => {
-    event.preventDefault();
-    target.classList.toggle('hidden');
-  });
-});
+/* augment a paragraph with basic text data */
+const augment = (text, paragraph) =>
+  Object.assign({ section: text.label, reference: text.pages }, paragraph);
 
-dom.sections.forEach((div) => {
-  const id = div.getAttribute('data-section');
-  const section = sections[id];
-  const css = id.split('-')[0];
-  if (section.title) { // remove this later - every section should have a title
-    div.innerHTML = format.title(section.title[1]);
-  } else {
-    div.innerHTML = '';
-  }
-  div.innerHTML += section.paragraphs.map(format.page.bind(null, section)).join('');
-  div.classList.add(css);
-  div.classList.remove('hidden');
-});
+/* the basic unit of display */
+const block = (content, meta, id) =>
+  `<div class="block" id="${id || ''}">
+    <div class="meta">${meta || ''}</div>
+    <div class="content">${content}</div>
+  </div>`;
 
-/*
-const doSearch = () => {
-  var hits = [];
-  if (dom.query.value.length > 0) {
-    dom.hits.innerHTML = '<p>Searching...</p>';
-    dom.results.classList.remove('hidden');
-    dom.close.classList.remove('hidden');
-    hits = search(dom.query.value);
-    if (hits.length > 0) {
-      dom.hits.innerHTML = hits.map(format.search.bind(null, dom.query.value)).join('');
-    } else {
-      dom.hits.innerHTML = '<p>No results.</p>';
-    }
-  }
+/* turn markdown-style text into pretty HTML */
+const pretty = text =>
+  text.replace(/_(.*?)_/g, '<em>$1</em>')
+    .replace(/~(.*?)~/g, '<span class="wide">$1</span>')
+    .replace(/\*(.*?)\*/g, '<span class="sc">$1</span>')
+    .replace(/\^(.*?)\^/g, '<span class="dc">$1</span>')
+    .replace(/\$(.*?)\$/g, '<span class="greek">$1</span>')
+    .replace(/`(.*?)`/g, '<blockquote>$1</blockquote>')
+    .replace(/\/\//g, '<br>')
+    .replace(/¬/g, '<span class="tab"></span>')
+    .replace(/\[(\d+?)\]/g, '<sup>[$1]</sup>')
+    .replace(/\|/g, '<span class="page-break">|</span>')
+    .replace(/\[#\d+?\]/g, '')
+    .replace(/\[\-(.*?) \+(.*?) @(.*?)\]/g, '<del>$1</del> <ins title="$3">$2</ins>')
+    .replace(/\[\-(.*?) @(.*?)\]/g, '<del title="$2">$1</del>')
+    .replace(/\[\+(.*?) @(.*?)\]/g, '<ins title="$2">$1</ins>');
+
+/* title block */
+const titleBlock = title => title ? block(pretty(title)) : '';
+
+/* absolute path to a section (based on its id or label) */
+const url = id => `/texts/${id.toLowerCase().replace(/(\.|-)/g, '/')}`;
+
+/* paragraph label */
+const label = paragraph => `${paragraph.section}.${paragraph.id}`.replace('.', ' ');
+
+/* paragraph pages */
+const pages = paragraph => paragraph.pages ? `, ${paragraph.reference} ${paragraph.pages}` : '';
+
+/* paragraph reference (label plus pages, with a hyperlink) */
+const ref = paragraph =>
+  `<a href="${url(paragraph.section)}/#${paragraph.id}">${label(paragraph)}${pages(paragraph)}</a>`;
+
+/* paragraph block */
+const paraBlock = paragraph =>
+  block(`<p class="${paragraph.type}">${pretty(paragraph.text)}</p>`, ref(paragraph), paragraph.id);
+
+/* paragraph blocks (from array of paragraphs, plus titles) */
+const paraBlocks = paragraphs =>
+  paragraphs.map(paragraph => titleBlock(paragraph.title) + paraBlock(paragraph)).join('');
+
+/* table of contents block */
+const tocBlock = ids => block(`<ul>${ids.map(id => `<li><a href="${url(id)}">${texts[id].title[1]}</a></li>`).join('')}</ul>`);
+
+/* text display */
+const display = (text) => text.paragraphs
+  ? (titleBlock(text.title[2]) + paraBlocks(text.paragraphs.map(augment.bind(null, text))))
+  : (titleBlock(text.title[2]) + tocBlock(text.texts));
+
+/* text paragraphs (for searching) */
+const paragraphs = text =>
+  text.paragraphs
+    ? text.paragraphs.map(augment.bind(null, text))
+    : text.texts.map(x => paragraphs(texts[x])).reduce((y, z) => y.concat(z), []);
+
+/* turn markdown-style text into plain text (for searching) */
+const plain = text =>
+  text.replace(/_/g, '')
+    .replace(/~/g, '')
+    .replace(/\*/g, '')
+    .replace(/\^/g, '')
+    .replace(/\$/g, '')
+    .replace(/`/g, '')
+    .replace(/\/\//g, ' ')
+    .replace(/¬/g, '')
+    .replace(/\[#\d+?\]/g, '')
+    .replace(/\[(\d+?)\]/g, '')
+    .replace(/<\/>/g, '')
+    .replace(/\|/g, '')
+    .replace(/\[\-(.*?) \+(.*?) @(.*?)\]/g, '$2')
+    .replace(/\[\-(.*?) @(.*?)\]/g, '')
+    .replace(/\[\+(.*?) @(.*?)\]/g, '$1')
+    .replace(/æ/ig, 'ae')
+    .replace(/œ/ig, 'oe')
+    .replace(/Œ/g, 'OE')
+    .replace(/[“|”]/g, '')
+    .replace(/’/g, '\'');
+
+/* regular expression (from user search query input) */
+const regex = query => new RegExp(`\\b(${query})\\b`, 'gi');
+
+/* paragraphs matching user's search query */
+const search = (paragraphs, query) =>
+  paragraphs.filter(paragraph => plain(paragraph.text).match(regex(query)));
+
+/* display paragraph matching user's search query */
+const result = (query, paragraph) =>
+  block(`<p class="${paragraph.type}">${plain(paragraph.text).replace(regex(query), '<mark>$1</mark>')}</p>`, ref(paragraph));
+
+/* display all paragraphs matching user's search query */
+const results = (text, query) => {
+  const haystack = paragraphs(text);
+  const hits = search(haystack, query);
+  const count = block('', `Query matched in ${hits.length} of ${haystack.length} paragraphs.`);
+  return count + hits.map(result.bind(null, query)).join('');
 };
 
-dom.toggles.forEach((x) => {
-  const id = x.getAttribute('data-toggle');
-  if (id === 'all') {
-    x.checked = texts.length === texts.filter(text => options.get(text.id)).length;
-    x.addEventListener('change', (event) => {
-      texts.forEach(text => options.set(text.id, x.checked));
-      dom.toggles.forEach(y => y.checked = x.checked);
-      doSearch();
-    });
-  } else {
-    x.checked = options.get(id);
-    x.addEventListener('change', (event) => {
-      options.toggle(id);
-      doSearch();
-    });
-  }
-});
+/* breadcrumb for top of toolbox */
+const breadcrumb = text =>
+  (text.parent === undefined)
+    ? `<h2><a href="${url(text.label)}">${text.title[0]}</a></h2>`
+    : `${breadcrumb(texts[text.parent])}<h2><a href="${url(text.label)}">${text.title[0]}</a></h2>`;
 
-if (dom.search) {
-  dom.search.addEventListener('submit', (event) => {
-    event.preventDefault();
-    doSearch();
-  });
-  dom.search.addEventListener('reset', (event) => {
-    dom.results.classList.add('hidden');
-    dom.close.classList.add('hidden');
-  });
+/* bootstrap */
+if ($('toolbox') && $('textbox')) {
+  if (text) {
+    $('title').innerHTML = breadcrumb(text);
+    $('cog').addEventListener('click', (event) => {
+      event.preventDefault();
+      $('options').classList.toggle('hidden');
+    });
+    $('search').addEventListener('submit', (event) => {
+      event.preventDefault();
+      if ($('query').value.length > 0) {
+        $('close').classList.remove('hidden');
+        $('textbox-body').innerHTML = results(text, $('query').value);
+      }
+    });
+    $('search').addEventListener('reset', (event) => {
+      $('close').classList.add('hidden');
+      $('textbox-body').innerHTML = display(text);
+    });
+    $('textbox-body').innerHTML = display(text);
+  } else {
+    $('textbox-body').innerHTML = '<p>Bad reference.</p>';
+  }
+  $('toolbox').classList.remove('hidden');
+  $('textbox').classList.remove('hidden');
 }
-*/
