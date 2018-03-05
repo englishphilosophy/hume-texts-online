27e2ed18c98443d0cc0d2567e11db1be0458da62
Amyas Merivale <merivale@gmail.com>
2018-03-05 15:25:11 +0100

    various tweaks

--- assets/hume.js
+++ assets/hume.js
@@ -23,20 +23,18 @@ const options = (() => {
     localStorage.setItem(item, JSON.stringify(value));
   const get = item =>
     JSON.parse(localStorage.getItem(item));
-  if (get('original-edition') === null) set('original-edition', false);
-  if (get('show-breaks') === null) set('show-breaks', false);
-  if (get('show-changes') === null) set('show-changes', false);
-  if (get('simple-search') === null) set('simple-search', true);
-  if (get('ignore-punctuation') === null) set('ignore-punctuation', true);
-  if (get('match-related') === null) set('match-related', true);
+  if (get('advanced') === null) set('advanced', false);
+  if (get('edited') === null) set('edited', true);
+  if (get('changes') === null) set('changes', false);
+  if (get('breaks') === null) set('breaks', false);
   return { set: set, get: get };
 })();
 
 const prepare = ((data, options) => {
   const edition = content =>
-    options.get('original-edition')
-      ? content.replace(/<ins( title='(.*?)')?>(.*?)<\/ins>/g, '')
-      : content.replace(/<del( title='(.*?)')?>(.*?)<\/del>/g, '');
+    options.get('edited')
+      ? content.replace(/<del( title='(.*?)')?>(.*?)<\/del>/g, '')
+      : content.replace(/<ins( title='(.*?)')?>(.*?)<\/ins>/g, '');
   const rich = content =>
     edition(content).replace(/<sup>(.*?)<\/sup>/g, '')
       .replace(/<span class='page-break'>\|<\/span>/g, '')
@@ -52,30 +50,24 @@ const prepare = ((data, options) => {
 
 const search = ((options, prepare) => {
   const searchable = content =>
-    (options.get('simple-search') && options.get('ignore-punctuation'))
-      ? prepare.plainer(content)
-      : prepare.plain(content);
-  const simplify = query => {
-    var simplified = query;
-    if (options.get('ignore-punctuation')) {
-      simplified = prepare.plainer(simplified);
-    }
-    if (options.get('match-related')) {
-      simplified = simplified.replace(/(ct|x)ion\b/g, '(ction|xion)')
-        .replace(/\bcould\b/g, 'coul|\'d')
-        .replace(/\bshould\b/g, 'shoul|\'d')
-        .replace(/\bwould\b/g, 'woul|\'d')
-        .replace(/ied\b/g, '((ied)|(y\'d))')
-        .replace(/ed\b/g, '(ed|\'d)')
-        .replace(/though\b/g, 'tho((oug)|\')')
-        .replace(/\bbetw((ixt)|(een))\b/g, 'betw((ixt)|(een))')
-        .replace(/\bdispatch(t|(ed))\b/g, 'dispatch(t|(ed))')
-        .replace(/\bit ((is)|(was)|(were))\b/g, '((it $1)|(\'t$1))');
-    }
-    return simplified;
-  };
+    (options.get('advanced'))
+      ? prepare.plain(content)
+      : prepare.plainer(content);
+  const simplify = query =>
+    prepare.plainer(query).replace(/(ct|x)ion\b/g, '(ction|xion)')
+      .replace(/\bcould\b/g, 'coul|\'d')
+      .replace(/\bshould\b/g, 'shoul|\'d')
+      .replace(/\bwould\b/g, 'woul|\'d')
+      .replace(/ied\b/g, '((ied)|(y\'d))')
+      .replace(/ed\b/g, '(ed|\'d)')
+      .replace(/though\b/g, 'tho((oug)|\')')
+      .replace(/\bbetw((ixt)|(een))\b/g, 'betw((ixt)|(een))')
+      .replace(/\bdispatch(t|(ed))\b/g, 'dispatch(t|(ed))')
+      .replace(/\bit ((is)|(was)|(were))\b/g, '((it $1)|(\'t$1))');
   const regex = query =>
-    options.get('simple-search') ? new RegExp(`(${simplify(query)})`, 'gi') : new RegExp(`(${query})`, 'gi');
+    options.get('advanced')
+      ? new RegExp(`(${query})`, 'gi')
+      : new RegExp(`(${simplify(query)})`, 'gi');
   const filter = (blocks, query) =>
     blocks.filter(x => searchable(x.content).match(regex(query)));
   return { regex: regex, filter: filter };
@@ -119,29 +111,36 @@ const page = ((data) => {
     $$('.tab-pane').forEach(togglePane.bind(null, what));
   };
   const clickTab = (event) => {
-      show(event.currentTarget.getAttribute('data-show'));
-    };
+    show(event.currentTarget.getAttribute('data-show'));
+  };
   const submitSearch = (event) => {
     event.preventDefault();
-    const text = data.text($('tools').getAttribute('data-text'));
-    const blocks = data.blocks(text);
-    const hits = search.filter(blocks, $('query').value);
-    $('results').innerHTML = display.summary(hits, blocks)
-      + display.blocks(hits, $('query').value);
+    if ($('query').value.length > 0) {
+      const text = data.text($('tools').getAttribute('data-text'));
+      const blocks = data.blocks(text);
+      const hits = search.filter(blocks, $('query').value);
+      $('results').innerHTML = display.summary(hits, blocks) + display.blocks(hits, $('query').value);
+    } else {
+      $('results').innerHTML = `<div class="meta"><p>Enter your query into the search box above, and press ENTER or click the search icon to see results.</p></div>`;
+    }
     show('results');
   };
   const updateText = () => {
-    if (options.get('original-edition')) {
-      $('text').classList.add('original');
-    } else {
+    if (options.get('edited')) {
       $('text').classList.remove('original');
+      $('changes').disabled = false;
+      $('changes').parentElement.classList.remove('disabled');
+    } else {
+      $('text').classList.add('original');
+      $('changes').disabled = true;
+      $('changes').parentElement.classList.add('disabled');
     }
-    if (options.get('show-breaks')) {
+    if (options.get('breaks')) {
       $('text').classList.add('breaks');
     } else {
       $('text').classList.remove('breaks');
     }
-    if (options.get('show-changes')) {
+    if (options.get('changes')) {
       $('text').classList.add('changes');
     } else {
       $('text').classList.remove('changes');
@@ -151,41 +150,24 @@ const page = ((data) => {
     if ($('tools')) {
       $$('.tab').forEach(x => x.addEventListener('click', clickTab));
       $('search').addEventListener('submit', submitSearch);
-      $('original-edition').checked = options.get('original-edition');
-      $('edited-edition').checked = !options.get('original-edition');
-      $('show-breaks').checked = options.get('show-breaks');
-      $('show-changes').checked = options.get('show-changes');
-      $('simple-search').checked = options.get('simple-search');
-      $('advanced-search').checked = !options.get('simple-search');
-      $('ignore-punctuation').checked = options.get('ignore-punctuation');
-      $('match-related').checked = options.get('match-related');
-      $('original-edition').addEventListener('change', () => {
-        options.set('original-edition', $('original-edition').value);
+      $('advanced').checked = options.get('advanced');
+      $('edited').checked = options.get('edited');
+      $('breaks').checked = options.get('breaks');
+      $('changes').checked = options.get('changes');
+      $('advanced').addEventListener('change', () => {
+        options.set('advanced', $('advanced').checked);
+      });
+      $('edited').addEventListener('change', () => {
+        options.set('edited', $('edited').checked);
         updateText();
       });
-      $('edited-edition').addEventListener('change', () => {
-        options.set('original-edition', !$('original-edition').value);
+      $('breaks').addEventListener('change', () => {
+        options.set('breaks', $('breaks').checked);
         updateText();
       });
-      $('show-breaks').addEventListener('change', () => {
-        options.set('show-breaks', $('show-breaks').value);
+      $('changes').addEventListener('change', () => {
+        options.set('changes', $('changes').checked);
         updateText();
-      });
-      $('show-changes').addEventListener('change', () => {
-        options.set('show-changes', $('show-changes').value);
-        updateText();
-      });
-      $('simple-search').addEventListener('change', () => {
-        options.set('simple-search', $('simple-search').value);
-      });
-      $('advanced-search').addEventListener('change', () => {
-        options.set('advanced-search', $('advanced-search').value);
-      });
-      $('ignore-punctuation').addEventListener('change', () => {
-        options.set('ignore-punctuation', $('ignore-punctuation').value);
-      });
-      $('match-related').addEventListener('change', () => {
-        options.set('match-related', $('match-related').value);
       });
       updateText();
     }
